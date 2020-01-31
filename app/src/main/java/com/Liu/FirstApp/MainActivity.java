b8b83052241564169da6829f769cf191d705bdc5
Kevin Liu <dethsandwich@gmail.com>
2020-01-31 13:54:26 -0500

    Initial commit

---
+++ app/src/main/java/com/Liu/FirstApp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.Liu.FirstApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
